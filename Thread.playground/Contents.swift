3ff45a6df587bf8dfc1ce756291b7d378851a711
Raiyan Sharif <raiyan.sharif.1234@gmail.com>
2024-05-13 17:25:22 +0600

    custom thread

---
+++ Thread.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+class CustomThread{
+    func createThread(){
+        let thread: Thread = Thread(target: self, selector: #selector(threadSelector), object: nil)
+        thread.start()
+    }
+
+    @objc func threadSelector(){
+        print("Custom Thread in action")
+    }
+}
+
+let customThread = CustomThread()
+customThread.createThread()
